1bb4a5a32f3e0a14ccc2128a35c9aa3ad4cc5dbe
fiflokonon <fiflokonon@gmail.com>
2024-11-01 09:18:32 +0100

    OK

--- 0x00-Webpack/task_3/webpack.config.js
+++ 0x00-Webpack/task_3/webpack.config.js
@@ -1,33 +1,63 @@
-const path = require("path");
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-    mode: "production",
+    plugins: [
+        new HTMLWebpackPlugin({
+            filename: './index.html',
+        }),
+        new CleanWebpackPlugin(),
+    ],
+    devtool: 'inline-source-map',
+    mode: 'development',
     entry: {
-        main: path.resolve(__dirname, "./js/dashboard_main.js"),
+        header: {
+            import: './modules/header/header.js',
+            dependOn: 'shared',
+        },
+        body: {
+            import: './modules/body/body.js',
+            dependOn: 'shared',
+        },
+        footer: {
+            import: './modules/footer/footer.js',
+            dependOn: 'shared',
+        },
+        shared: 'jquery',
     },
     output: {
-        path: path.resolve(__dirname, "public"),
-        filename: "bundle.js",
+        path: path.resolve(__dirname, 'public'),
+        filename: '[name].bundle.js',
+    },
+    optimization: {
+        splitChunks: {
+            chunks: 'all',
+        },
+    },
+    devServer: {
+        static: path.join(__dirname, './public'),
+        open: true,
+        port: 8564,
     },
     performance: {
-        hints: false,
-        maxEntrypointSize: 512000,
-        maxAssetSize: 512000,
+        maxAssetSize: 1000000,
     },
     module: {
         rules: [
             {
                 test: /\.css$/i,
-                use: ["style-loader", "css-loader"],
+                use: ["css-loader", "style-loader"],
             },
             {
-                test: /\.(gif|png|jp?g|svg)$/i,
+                test: /\.(?:ico|gif|png|jpe?g|svg)$/i,
+                type: 'asset/resource',
                 use: [
                     "file-loader",
                     {
                         loader: "image-webpack-loader",
                         options: {
-                            bypassOnDebug: true,
+                            bypassingOnDebug: true,
                             disable: true,
                         },
                     },
